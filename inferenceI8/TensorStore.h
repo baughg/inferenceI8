478650b6634de384372ab33a9e6423e560c1b5fd
Gary Baugh <baughg@tcd.ie>
2020-04-08 08:58:19 +0100

    fetching input for reshape

--- inferenceI8/TensorStore.h
+++ inferenceI8/TensorStore.h
@@ -18,11 +18,16 @@ namespace GB {
 		template<typename D_Ty, std::size_t cstep>
 		friend TensorStore<D_Ty, cstep> from_Tensor(const Tensor &t1);
 		void reshape_for_compute(const ConvParam &param, Type type);
+		bool get_element(const int &elem, const int &k, Data_Ty* &p_data) const;
+		void set_stride();
 	private:
 		using Container = std::vector<Data_Ty>;
 		Container data_ {};
-		TensorShape shape_{};
+		TensorShape shape_ {};
 		const uint32_t channel_step_{ chnstep };
+		int elements_ {};
+		int k_stride_ {};
+		Data_Ty* data_ptr_{ nullptr };
 	};		
 }
 
